579fd543a38d636bb107d82368130be24136e4ca
zerodoc <danielc@smallwoodhome.com>
2021-02-04 16:54:36 -0600

    fix vertex description set issue

--- src/renderer/vertex_buffer.hpp
+++ src/renderer/vertex_buffer.hpp
@@ -2,24 +2,23 @@
 
 #include "build_order.hpp"
 #include "device.hpp"
-struct Vertex{
+struct Vertex {
     glm::vec4 pos;
     glm::vec3 color;
     glm::vec2 texCoord;
 
     static VkVertexInputBindingDescription getBindingDescription()
     {
-        VkVertexInputBindingDescription binding_description = {};
+        VkVertexInputBindingDescription binding_description;
         binding_description.binding = 0;
         binding_description.stride = sizeof(Vertex);
         binding_description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        printfv("%d\n",binding_description.inputRate);
         return binding_description;
     }
 
     static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
-    {        
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+    {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions;
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
         attributeDescriptions[0].format = VK_FORMAT_R32G32B32A32_SFLOAT;
@@ -30,10 +29,10 @@ struct Vertex{
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[1].offset = offsetof(Vertex, color);
 
-        attributeDescriptions[2].binding = 0;
+        /* attributeDescriptions[2].binding = 0;
         attributeDescriptions[2].location = 2;
         attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
+        attributeDescriptions[2].offset = offsetof(Vertex, texCoord); */
         return attributeDescriptions;
     }
 };
--- src/shader/basic.frag
+++ src/shader/basic.frag
@@ -1,15 +1,17 @@
 #version 450
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(set = 1, binding = 0) uniform sampler2D texSampler;
+// layout(binding = 0) uniform sampler2D texSampler;
 
 layout(location = 0) in vec3 fragColor;
-layout(location = 0) in vec2 fragTex;
+// layout(location = 1) in vec2 fragTex;
 
 layout(location = 0) out vec4 outColor;
 
 void main() {
-    vec4 texel = texture(texSampler, fragTex);
-    outColor.rgb = texel.rgb * fragColor.rbg * fragColor.a * texel.a;
-    outColor.a = texel.a * fragColor.a;
+    // vec4 texel = texture(texSampler, fragTex);
+    // outColor.rgb = texel.rgb * fragColor.rbg * fragColor.a * texel.a;
+    // outColor.a = texel.a * fragColor.a;
+    // outColor = vec4(fragColor * texture(texSampler, fragTex).rgb, 1.0);
+    outColor = vec4(fragColor, 1.0) * 1.5;
 }
