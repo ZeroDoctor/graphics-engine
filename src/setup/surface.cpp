a52d694a5defc4b698827f584fb55b40fb1f77df
zerodoctor <danielrcastro10@gmail.com>
2021-02-16 14:14:22 -0600

    add surface support for xcb

--- src/setup/surface.cpp
+++ src/setup/surface.cpp
@@ -1,7 +1,164 @@
 #include "surface.hpp"
 
-Surface::Surface(uint32_t width, uint32_t height) 
+VulkanSurface::VulkanSurface(VulkanInstance* instance, WindowSettings settings, uint32_t width, uint32_t height) 
 {
     m_width = width;
     m_height = height;
+    m_instance = instance;
+    m_settings = settings;
+	printfi("Setting up Window\n");
+    setupWindow();
+	printfi("Creating Surface\n");
+    initSurface();
 }
+VulkanSurface::~VulkanSurface()
+{
+	if(m_surface != NULL) {
+		printfi("-- Destroying Surface...\n");
+    	vkDestroySurfaceKHR(m_instance->GetInstance(), m_surface, nullptr);
+	}
+	
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+    xcb_destroy_window(m_connection, m_window);
+	if(m_connection != NULL) {
+		xcb_disconnect(m_connection);
+	}
+#endif
+}
+
+VkSurfaceKHR VulkanSurface::GetSurface() 
+{
+	return m_surface;
+}
+
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+static xcb_intern_atom_reply_t* intern_atom_helper(xcb_connection_t *conn, bool only_if_exists, const char *str) 
+{
+    xcb_intern_atom_cookie_t cookie = xcb_intern_atom(conn, only_if_exists, strlen(str), str);
+	return xcb_intern_atom_reply(conn, cookie, NULL);
+}
+
+void VulkanSurface::setupWindow()
+{
+    // * connect to X server
+    const xcb_setup_t *setup;
+    xcb_screen_iterator_t iter;
+    int scr;
+
+    m_connection = xcb_connect(NULL, &scr);
+    assert(m_connection);
+    if(xcb_connection_has_error(m_connection))
+    {
+        printfe("Failed to find a compatible Vulkan ICD! (aka xcb connection failed)");
+        fflush(stdout);
+        exit(1);
+    }
+
+    setup = xcb_get_setup(m_connection);
+    iter = xcb_setup_roots_iterator(setup);
+    while(scr-- > 0) {
+        xcb_screen_next(&iter);
+    }
+    m_screen = iter.data;
+
+    // * generate window
+    uint32_t value_mask, value_list[32];
+
+    m_window = xcb_generate_id(m_connection);
+    value_mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
+    value_list[0] = m_screen->black_pixel;
+
+    value_list[1] =
+		XCB_EVENT_MASK_KEY_RELEASE |
+		XCB_EVENT_MASK_KEY_PRESS |
+		XCB_EVENT_MASK_EXPOSURE |
+		XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+		XCB_EVENT_MASK_POINTER_MOTION |
+		XCB_EVENT_MASK_BUTTON_PRESS |
+		XCB_EVENT_MASK_BUTTON_RELEASE;
+
+	if (m_settings.fullscreen)
+	{
+		m_width = m_dest_width = m_screen->width_in_pixels;
+		m_height = m_dest_height = m_screen->height_in_pixels;
+	}
+
+    xcb_create_window(m_connection,
+		XCB_COPY_FROM_PARENT,
+	    m_window, m_screen->root,
+		0, 0, m_width, m_height, 0,
+		XCB_WINDOW_CLASS_INPUT_OUTPUT,
+		m_screen->root_visual,
+		value_mask, value_list
+    );
+
+	/* Magic code that will send notification when window is destroyed */
+	xcb_intern_atom_reply_t* reply = intern_atom_helper(m_connection, true, "WM_PROTOCOLS");
+	m_atom_wm_delete_window = intern_atom_helper(m_connection, false, "WM_DELETE_WINDOW");
+
+	xcb_change_property(m_connection, XCB_PROP_MODE_REPLACE,
+		m_window, (*reply).atom, 4, 32, 1,
+		&(*m_atom_wm_delete_window).atom);
+
+	std::string windowTitle = m_settings.title;
+	xcb_change_property(m_connection, XCB_PROP_MODE_REPLACE,
+		m_window, XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
+		m_settings.title.size(), windowTitle.c_str());
+
+	free(reply);
+
+	/**
+	 * Set the WM_CLASS property to display
+	 * title in dash tooltip and application menu
+	 * on GNOME and other desktop environments
+	 */
+	std::string wm_class;
+    std::string name = m_settings.title;
+    std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c){ return std::tolower(c); });
+	wm_class = wm_class.insert(0, name);
+	wm_class = wm_class.insert(name.size(), 1, '\0');
+	wm_class = wm_class.insert(name.size() + 1, m_settings.title);
+	wm_class = wm_class.insert(wm_class.size(), 1, '\0');
+	xcb_change_property(m_connection, XCB_PROP_MODE_REPLACE, m_window, XCB_ATOM_WM_CLASS, XCB_ATOM_STRING, 8, wm_class.size() + 2, wm_class.c_str());
+
+	if (m_settings.fullscreen)
+	{
+		xcb_intern_atom_reply_t *atom_wm_state = intern_atom_helper(m_connection, false, "_NET_WM_STATE");
+		xcb_intern_atom_reply_t *atom_wm_fullscreen = intern_atom_helper(m_connection, false, "_NET_WM_STATE_FULLSCREEN");
+		xcb_change_property(m_connection,
+				XCB_PROP_MODE_REPLACE,
+				m_window, atom_wm_state->atom,
+				XCB_ATOM_ATOM, 32, 1,
+				&(atom_wm_fullscreen->atom));
+		free(atom_wm_fullscreen);
+		free(atom_wm_state);
+	}
+
+    // * display the window
+	xcb_map_window(m_connection, m_window);
+    xcb_flush(m_connection);
+}
+
+void VulkanSurface::initSurface()
+{
+    VkXcbSurfaceCreateInfoKHR info = init::surface_info(m_connection, m_window);
+
+    ErrorCheck(
+		vkCreateXcbSurfaceKHR(
+            m_instance->GetInstance(), 
+            &info, nullptr, 
+            &m_surface
+        ), "Create XCB Surface"
+    );
+}
+#elif defined(VK_USE_PLATFORM_WIN32_KHR)	
+void VulkanSurface::SetupWindow()
+{
+
+}
+
+void VulkanSurface::InitSurface()
+{
+
+}
+#endif
--- src/setup/surface.hpp
+++ src/setup/surface.hpp
@@ -1,16 +1,42 @@
 #pragma once
 
 #include "build_order.hpp"
+#include "instance.hpp"
 
-class Surface 
+struct WindowSettings {
+    bool validation = false;
+    bool fullscreen = false;
+    bool vsync = false;
+    bool overlay = false;
+    std::string title;
+};
+
+class VulkanSurface 
 {
 public:
-    Surface(uint32_t, uint32_t);
-    ~Surface();
+    VulkanSurface(VulkanInstance*, WindowSettings, uint32_t, uint32_t);
+    ~VulkanSurface();
 
+    VkSurfaceKHR GetSurface();
 
 private:
     uint32_t m_width;
     uint32_t m_height;
-    GLFWwindow* m_window;
+    uint32_t m_dest_width;
+	uint32_t m_dest_height;
+
+    VulkanInstance* m_instance;
+    VkSurfaceKHR m_surface;
+    WindowSettings m_settings;
+
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+	bool m_quit = false;
+	xcb_connection_t *m_connection;
+	xcb_screen_t *m_screen;
+	xcb_window_t m_window;
+	xcb_intern_atom_reply_t *m_atom_wm_delete_window;
+#endif
+
+    void setupWindow();
+    void initSurface();
 };
