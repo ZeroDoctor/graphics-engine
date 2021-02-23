740ce123765dde0786562a3c5ec775dc3ea6db76
zerodoctor <>
2021-02-23 16:54:52 -0600

    add surface support details structure

--- src/init/queue_family.hpp
+++ src/init/queue_family.hpp
@@ -4,7 +4,13 @@
 
 struct QueueFamilyIndices 
 {
-    uint32_t graphics_indices = UINT32_MAX;
-    uint32_t compute_indices = UINT32_MAX;
-    uint32_t present_indices = UINT32_MAX;
+    uint32_t graphics_index = UINT32_MAX;
+    uint32_t compute_index = UINT32_MAX;
+    uint32_t present_index = UINT32_MAX;
+};
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> present_modes;
 };
